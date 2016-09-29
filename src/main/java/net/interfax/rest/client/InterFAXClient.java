51e3c31fc0c2459af1fd528211cd0d4858d7483e
Anurag Kapur <anurag@beancrunch.com>
2016-09-29 18:31:52 +0100

    Remove unused variable

--- src/main/java/net/interfax/rest/client/InterFAXClient.java
+++ src/main/java/net/interfax/rest/client/InterFAXClient.java
@@ -6,8 +6,6 @@ import java.io.File;
 
 public interface InterFAXClient {
 
-    String OUTBOUND_FAXES_PATH = "outbound/faxes?faxNumber=%s";
-
     public Response sendFax(final String faxNumber, final File fileToSendAsFax);
 
 }
