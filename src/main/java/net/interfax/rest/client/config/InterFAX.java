c60d0b5c9b2292556019986c56a1e5f87321449f
Anurag Kapur <anurag@beancrunch.com>
2016-10-01 14:28:31 +0100

    Split uri into scheme, hostname and port for testability
    ...using wiremock. Need to send wiremock requests to localhost.

--- src/main/java/net/interfax/rest/client/config/InterFAX.java
+++ src/main/java/net/interfax/rest/client/config/InterFAX.java
@@ -2,7 +2,29 @@ package net.interfax.rest.client.config;
 
 public class InterFAX {
 
+    private String scheme;
+
+    private String hostname;
+
     private String outboundFaxesEndpoint;
+
+    private String outboundDocumentsEndpoint;
+
+    public String getScheme() {
+        return scheme;
+    }
+
+    public void setScheme(final String scheme) {
+        this.scheme = scheme;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(final String hostname) {
+        this.hostname = hostname;
+    }
 
     public String getOutboundFaxesEndpoint() {
         return outboundFaxesEndpoint;
@@ -10,5 +32,13 @@ public class InterFAX {
 
     public void setOutboundFaxesEndpoint(final String outboundFaxesEndpoint) {
         this.outboundFaxesEndpoint = outboundFaxesEndpoint;
+    }
+
+    public String getOutboundDocumentsEndpoint() {
+        return outboundDocumentsEndpoint;
+    }
+
+    public void setOutboundDocumentsEndpoint(final String outboundDocumentsEndpoint) {
+        this.outboundDocumentsEndpoint = outboundDocumentsEndpoint;
     }
 }
