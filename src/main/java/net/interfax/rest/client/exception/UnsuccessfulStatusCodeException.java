68f6bf83e2dd52e68275bcbd268d323351fb0edf
Anurag Kapur <anurag@beancrunch.com>
2016-10-07 20:58:53 +0100

    Add attributes to capture status code and response body in custom exception

--- src/main/java/net/interfax/rest/client/exception/UnsuccessfulStatusCodeException.java
+++ src/main/java/net/interfax/rest/client/exception/UnsuccessfulStatusCodeException.java
@@ -2,9 +2,31 @@ package net.interfax.rest.client.exception;
 
 public class UnsuccessfulStatusCodeException extends Exception {
 
+    private int statusCode;
+    private String responseBody;
+
+    public UnsuccessfulStatusCodeException(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public UnsuccessfulStatusCodeException(int statusCode, String responseBody) {
+        this.statusCode = statusCode;
+        this.responseBody = responseBody;
+    }
 
     public UnsuccessfulStatusCodeException(String message) {
         super(message);
+    }
+
+    public UnsuccessfulStatusCodeException(String message, int statusCode, String responseBody) {
+        super(message);
+        this.statusCode = statusCode;
+        this.responseBody = responseBody;
+    }
+
+    public UnsuccessfulStatusCodeException(String message, int statusCode) {
+        super(message);
+        this.statusCode = statusCode;
     }
 
     public UnsuccessfulStatusCodeException(final String message, final Throwable cause) {
@@ -17,6 +39,22 @@ public class UnsuccessfulStatusCodeException extends Exception {
 
     public UnsuccessfulStatusCodeException(final String message, final Throwable cause, final boolean enableSuppression, final boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(final int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getResponseBody() {
+        return responseBody;
+    }
+
+    public void setResponseBody(final String responseBody) {
+        this.responseBody = responseBody;
     }
 
 }
