6357ba31dd82d55b36df6662fdb72af847b260d3
Anurag Kapur <anurag@beancrunch.com>
2016-10-16 23:10:18 +0100

    Add debug statement in release script to find out why the travic build fails

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,5 +3,10 @@
 if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST = "false" ]]; then
     git checkout master
     git remote add origin git@github.com:interfax/interfax-java.git
+    echo "===="
+    pwd
+    echo "===="
+    ls -ltr scripts/keys
+    echo "===="
     mvn release:prepare release:perform -Darguments="-Dgpg.defaultKeyring=false -Dgpg-keyname=54F168DD -Dgpg.passphrase=$GPGKEY_PASSPHRASE -Dgpg.publicKeyring=~/pubring.gpg -Dgpg.secretKeyring=~/secring.gpg -DskipTests=true -DskipTests=true" -B -Prelease --settings ~/settings.xml
 fi
