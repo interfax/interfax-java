c193179c5b4281b8d27a9b5b050437757a8f2dcf
Anurag Kapur <anurag@beancrunch.com>
2016-12-05 23:45:05 +0000

    Minor javadoc correction and formatting improvement

--- src/main/java/net/interfax/rest/client/InterFAX.java
+++ src/main/java/net/interfax/rest/client/InterFAX.java
@@ -68,7 +68,9 @@ public interface InterFAX {
      * @throws IOException
      * @see <a href="https://www.interfax.net/en/dev/rest/reference/2918">https://www.interfax.net/en/dev/rest/reference/2918</a>
      */
-    public APIResponse sendFax(final String faxNumber, final InputStream[] streamsToSendAsFax, final String fileNames[]) throws IOException;
+    public APIResponse sendFax(final String faxNumber,
+                               final InputStream[] streamsToSendAsFax,
+                               final String fileNames[]) throws IOException;
 
     /**
      * Send an array of files as a fax with additional {@link SendFaxOptions}
@@ -85,7 +87,7 @@ public interface InterFAX {
                                final Optional<SendFaxOptions> options) throws IOException;
 
     /**
-     * Send an array of files as a fax with additional {@link SendFaxOptions}
+     * Send an array of input streams as a fax with additional {@link SendFaxOptions}
      *
      * @param faxNumber        number to fax to
      * @param streamsToSendAsFax array of files to send as fax
