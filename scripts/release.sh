29783353b617b3aa62e5a365688224b18a0055a8
Ricky "Shake'n'Bake" Bobby <39415009+ricky-shake-n-bake-bobby@users.noreply.github.com>
2020-04-17 20:38:21 -0700

    Update release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,5 +3,5 @@
 if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST = "false" ]]; then
     git checkout master
     git remote add origin git@github.com:interfax/interfax-java.git
-    mvn release:prepare release:perform -Darguments="-Dgpg.defaultKeyring=false -Dgpg.keyname=705618AE -Dgpg.passphrase=$GPGKEY_PASSPHRASE -Dgpg.publicKeyring=~/pubring.gpg -Dgpg.secretKeyring=~/secring.gpg -DskipTests=true" -B -Prelease --settings ~/settings.xml
+    mvn release:prepare release:perform -Darguments="-Dgpg.defaultKeyring=false -Dgpg.keyname=705618AE -Dgpg.passphrase=$GPGKEY_PASSPHRASE -Dgpg.publicKeyring=~/pubring.gpg -Dgpg.secretKeyring=~/secring.gpg -DskipTests=true -Dmaven.javadoc.skip=true" -B -Prelease --settings ~/settings.xml
 fi
